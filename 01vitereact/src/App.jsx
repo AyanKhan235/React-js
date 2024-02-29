c67235e5a8b9bc240684458e03f349515410a78c
Ayan_khan_235 <babukhan640b@gmail.com>
2024-02-29 23:48:27 +0530

    create custome react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,10 +1,11 @@
 import Chai from './Chai'
 function App() {
-
+  const username="AYAN KHAN"
   return (
+    // /   fregment is that in this we can only write one tag but in one tag we can use multiple its called fragmentz
     <>
     <Chai/>
-    <h3>HEllo we are in Learning Phase</h3>
+    <h3>HEllo we are in Learning Phase {username}</h3>
     </>
   )
 }
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -1,11 +1,41 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import Chai from './chai.jsx'
 
-
+function  MyApp() {
+  return(
+    <div>
+      <h2>Custome React |</h2>
+    </div>
+  )
+  
+}
+// const reactElement={
+//   type:'a',
+//   props:{
+//       href:'http://google.com',
+//       target:'_blank'
+//   },
+//   children:'click me to visit'
+// }
+const anotherElement=(
+  <a href="http://google.com" target="_blank">
+    click me to visit
+  </a>
+)
+const reactElement=React.createElement(
+  'a',
+  {href:'http://google.com', target:'_blank'}, // it object is compulsary 
+  'click me to vsit our google'
+)
 ReactDOM.createRoot(document.getElementById('root')).render(
-//   fregment is that in this we can only write one tag but in one tag we can use multiple its called fragment
+
+  // <MyApp/>
+  // reactElement      // will not work
+  // anotherElement
+
+  // reactElement
   <App/>
+
  
 )
